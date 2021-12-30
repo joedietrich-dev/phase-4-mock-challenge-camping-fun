325f6549e4c1313e27f768071bc6459bfea58c38
joedietrich-dev <joe@joedietrich.net>
2021-12-29 21:54:13 -0500

    commit tree e3dc9c44cbc6766955ef99246e4d197815fa55dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'signups/create'
+  get 'activities/index'
+  get 'activities/destroy'
+  get 'campers/index'
+  get 'campers/show'
+  get 'campers/create'
+  get 'activities/index'
+  get 'activities/show'
+  get 'activities/create'
+  get 'camper/index'
+  get 'camper/show'
+  get 'camper/create'
+  resources :campers, only: [:index, :show, :create]
+  resources :activities, only: [:index, :destroy]
+  resources :signups, only: [:create]
 end
